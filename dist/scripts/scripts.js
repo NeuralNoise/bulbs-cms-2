eca07e23660e8f1890659ae580edefc2321adfd6
Andrew Kos <akos123@gmail.com>
2015-10-05 21:41:50 -0500

    Some content workflow cleanup, I also hate this file

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -9071,100 +9071,104 @@ angular.module('bulbsCmsApp')
 'use strict';
 
 angular.module('bulbsCmsApp')
-  .controller('ContentworkflowCtrl', function ($scope, $http, $modal, $window, moment,
-                                               VersionBrowserModalOpener, TemporaryUrlModalOpener,
-                                               TIMEZONE_NAME, PARTIALS_URL) {
-    $scope.TIMEZONE_LABEL = moment.tz(TIMEZONE_NAME).format('z');
+  .controller('ContentworkflowCtrl', [
+    '$scope', '$modal', 'moment', 'VersionBrowserModalOpener',
+      'TemporaryUrlModalOpener', 'TIMEZONE_NAME', 'PARTIALS_URL',
+    function ($scope, $modal, moment, VersionBrowserModalOpener,
+        TemporaryUrlModalOpener, TIMEZONE_NAME, PARTIALS_URL) {
 
-    $scope.trashContentModal = function (articleId) {
-      return $modal.open({
-        templateUrl: PARTIALS_URL + 'modals/confirm-trash-modal.html',
-        controller: 'TrashcontentmodalCtrl',
-        scope: $scope,
-        resolve: {
-          articleId: function () {
-            return articleId;
+      $scope.TIMEZONE_LABEL = moment.tz(TIMEZONE_NAME).format('z');
+
+      $scope.trashContentModal = function (articleId) {
+        return $modal.open({
+          templateUrl: PARTIALS_URL + 'modals/confirm-trash-modal.html',
+          controller: 'TrashcontentmodalCtrl',
+          scope: $scope,
+          resolve: {
+            articleId: function () {
+              return articleId;
+            }
           }
+        });
+      };
+
+      $scope.pubTimeModal = function (article) {
+        return $modal.open({
+          templateUrl: PARTIALS_URL + 'modals/publish-date-modal.html',
+          controller: 'PubtimemodalCtrl',
+          scope: $scope,
+          resolve: {
+            article: function () { return article; }
+          }
+        });
+      };
+
+      $scope.changelogModal = function (article) {
+        return $modal.open({
+          templateUrl: PARTIALS_URL + 'modals/changelog-modal.html',
+          controller: 'ChangelogmodalCtrl',
+          scope: $scope,
+          resolve: {
+            article: function () { return article; }
+          }
+        });
+      };
+
+      $scope.thumbnailModal = function (article) {
+        // open thumbnail modal along with its controller
+        return $modal.open({
+          templateUrl: PARTIALS_URL + 'modals/thumbnail-modal.html',
+          controller: 'ThumbnailModalCtrl',
+          scope: $scope,
+          resolve: {
+            article: function () { return article; }
+          }
+        });
+      };
+
+      $scope.sponsorModal = function (article) {
+        return $modal.open({
+          templateUrl: PARTIALS_URL + 'modals/sponsor-modal.html',
+          scope: $scope,
+          controller: 'SponsormodalCtrl',
+          resolve: {
+            article: function () { return article; }
+          }
+        });
+      };
+
+      $scope.versionBrowserModal = function (article) {
+        VersionBrowserModalOpener.open($scope, article);
+      };
+
+      $scope.temporaryUrlModal = function (article) {
+        TemporaryUrlModalOpener.open($scope, article);
+      };
+
+      $scope.descriptionModal = function (article) {
+        return $modal.open({
+          templateUrl: PARTIALS_URL + 'modals/description-modal.html',
+          controller: 'DescriptionModalCtrl',
+          scope: $scope,
+          size: 'lg',
+          resolve: {
+            article: function () { return article; }
+          }
+        });
+      };
+
+      $scope.getStatus = function (article) {
+        if (!article || !article.published) {
+          return 'unpublished';
+        } else if (moment(article.published) > moment()) {
+          return 'scheduled';
+        } else {
+          return 'published';
         }
-      });
-    };
+      };
 
-    $scope.pubTimeModal = function (article) {
-      return $modal.open({
-        templateUrl: PARTIALS_URL + 'modals/publish-date-modal.html',
-        controller: 'PubtimemodalCtrl',
-        scope: $scope,
-        resolve: {
-          article: function () { return article; }
-        }
-      });
-    };
-
-    $scope.changelogModal = function (article) {
-      return $modal.open({
-        templateUrl: PARTIALS_URL + 'modals/changelog-modal.html',
-        controller: 'ChangelogmodalCtrl',
-        scope: $scope,
-        resolve: {
-          article: function () { return article; }
-        }
-      });
-    };
-
-    $scope.thumbnailModal = function (article) {
-      // open thumbnail modal along with its controller
-      return $modal.open({
-        templateUrl: PARTIALS_URL + 'modals/thumbnail-modal.html',
-        controller: 'ThumbnailModalCtrl',
-        scope: $scope,
-        resolve: {
-          article: function () { return article; }
-        }
-      });
-    };
-
-    $scope.sponsorModal = function (article) {
-      return $modal.open({
-        templateUrl: PARTIALS_URL + 'modals/sponsor-modal.html',
-        scope: $scope,
-        controller: 'SponsormodalCtrl',
-        resolve: {
-          article: function () { return article; }
-        }
-      });
-    };
-
-    $scope.versionBrowserModal = function (article) {
-      VersionBrowserModalOpener.open($scope, article);
-    };
-
-    $scope.temporaryUrlModal = function (article) {
-      TemporaryUrlModalOpener.open($scope, article);
-    };
-
-    $scope.descriptionModal = function (article) {
-      return $modal.open({
-        templateUrl: PARTIALS_URL + 'modals/description-modal.html',
-        controller: 'DescriptionModalCtrl',
-        scope: $scope,
-        size: 'lg',
-        resolve: {
-          article: function () { return article; }
-        }
-      });
-    };
-
-    $scope.getStatus = function (article) {
-      if (!article || !article.published) {
-        return 'unpublished';
-      } else if (moment(article.published) > moment()) {
-        return 'scheduled';
-      } else {
-        return 'published';
-      }
-    };
-
-  });
+    }]
+  );
 
 'use strict';
 
