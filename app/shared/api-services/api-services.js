bfa5ce864525880c6c71d09ce8c70e28259ae5f8
Andrew Kos <akos@theonion.com>
2015-10-29 14:53:42 -0500

    removed unnecessary drf paged style, fixed custom search service

--- app/shared/api-services/api-services.js
+++ app/shared/api-services/api-services.js
@@ -5,11 +5,14 @@ angular.module('apiServices', [
   'restmod',
   'restmod.styles.drfPaged'
 ])
-  .config(function (API_URL_ROOT, restmodProvider) {
-    restmodProvider.rebase('DjangoDRFPagedApi', {
-      $config: {
-        style: 'BulbsApi',
-        urlPrefix: API_URL_ROOT
-      }
-    });
-  });
+  .config([
+    'API_URL_ROOT', 'restmodProvider',
+    function (API_URL_ROOT, restmodProvider) {
+      restmodProvider.rebase('DjangoDRFPagedApi', {
+        $config: {
+          style: 'BulbsApi',
+          urlPrefix: API_URL_ROOT
+        }
+      });
+    }
+  ]);
