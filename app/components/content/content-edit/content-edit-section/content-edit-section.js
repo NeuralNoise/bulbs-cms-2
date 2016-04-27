af921db6173de1ecad0e8fca993602b704b23f88
Andrew Kos <akos123@gmail.com>
2016-04-27 14:18:56 -0500

    Fix <content-edit-section> directive (#23)
    * fix content-edit-section directive
    
    * list error fixed

--- app/components/content/content-edit/content-edit-section/content-edit-section.js
+++ app/components/content/content-edit/content-edit-section/content-edit-section.js
@@ -3,12 +3,13 @@
 angular.module('content.edit.section', [
   'apiServices.section.factory',
   'bulbsCmsApp.settings',
+  'lodash',
   'utils',
   'uuid4'
 ])
   .directive('contentEditSection', [
-    'COMPONENTS_URL', 'Utils', 'uuid4',
-    function (COMPONENTS_URL, Utils, uuid4) {
+    '_', 'COMPONENTS_URL', 'Utils', 'uuid4',
+    function (_, COMPONENTS_URL, Utils, uuid4) {
       return {
         controller: [
           '$scope', 'Section',
@@ -21,15 +22,46 @@ angular.module('content.edit.section', [
               })
               .$asPromise();
             };
+
+            $scope.itemDisplayFormatter = function (section) {
+              return _.isObject(section) ? section.name : null;
+            };
+
+            $scope.itemValueFormatter = function (section) {
+              if (_.isObject(section)) {
+                return {
+                  id: section.id,
+                  name: section.name,
+                  $resolved: true
+                };
+              }
+
+              return null;
+            };
           }
         ],
-        link: function (scope) {
+        link: function (scope, element, attrs, ngModelCtrl) {
           scope.uuid = uuid4.generate();
+
+          if (ngModelCtrl) {
+
+            scope.ngModel = ngModelCtrl;
+
+            ngModelCtrl.$render = function () {
+              if (_.isObject(ngModelCtrl.$modelValue) && !scope.initialValue) {
+                scope.initialValue = scope.itemDisplayFormatter(ngModelCtrl.$modelValue);
+              }
+            };
+
+            scope.onSelect = function () {
+              ngModelCtrl.$commitViewValue();
+            };
+          }
         },
         restrict: 'E',
+        require: 'ngModel',
         scope: {
-          article: '=',
-          onSelect: '&'
+          onSelect: '&campaignAutocompleteOnSelect' // selection handler for auto completions
         },
         templateUrl: Utils.path.join(
           COMPONENTS_URL,
